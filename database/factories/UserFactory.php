e73d2b9f85785b072e57a1cb05fd2909c3de670c
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2020-01-15 23:37:18 +0000

    UserFactory修正

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -9,6 +9,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'user_name' => str_random(5),
         'email' => $faker->unique()->safeEmail,
+        'password'  => bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
 });
