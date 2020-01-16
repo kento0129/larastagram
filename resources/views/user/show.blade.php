29a2a50a3602bcd31d7ec2be3ce1f0d5bb85ddb8
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2020-01-16 20:37:13 +0000

    ユーザー詳細画面修正

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -28,7 +28,7 @@
             </div>
           @endif
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog" role="document">
+          <div class="modal-dialog setting-modal-margin" role="document">
             <div class="modal-content">
               <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">設定</h5>
